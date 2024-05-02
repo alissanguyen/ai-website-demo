3e6da8e65f95dd61f5aa0f4ee42f201c9a21d9a7
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-01 21:13:05 -0700

    Add input validation to prevent client-side attacks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import {Spinner} from "@nextui-org/react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <Spinner size="md" color="secondary" />
 
     </main>
   );
