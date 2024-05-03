5af6d1f7dd7ea90aa406ccc15869d4aee2668cb8
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-02 20:47:14 -0700

    Update next config to read local env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { config } from "dotenv";
+config();
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Add html-loader for HTML files
+    config.module.rules.push({
+      test: /\.html$/,
+      use: "html-loader",
+    });
+
+    // Exclude @mapbox/node-pre-gyp module from being processed by Webpack
+    config.externals = config.externals || {};
+    config.externals["@mapbox/node-pre-gyp"] = "@mapbox/node-pre-gyp";
+
+    return config;
+  },
+};
 
 export default nextConfig;
