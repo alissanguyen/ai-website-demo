4295c7e007fece3318d5ec550653a4807086dffb
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-02 20:46:54 -0700

    Install supabase/ssr

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,24 +1,24 @@
-import { Model } from "@/types";
+import { Model } from "@/types/types";
 
 export const models: Model[] = [
-    {
-        id: "image",
-        name: "Image Generation"
-    },
-    {
-        id: "imageUpgraded",
-        name: "Image Generation Pro"
-    },
-    {
-        id: "textGeneration",
-        name: "Text Generation"
-    },
-    {
-        id: "textGenerationUpgraded",
-        name: "Text Generation Pro"
-    },
-    {
-        id: "imageClassification",
-        name: "Image Classification"
-    }
-]
+  {
+    id: "image",
+    name: "Image Generation"
+  },
+  {
+    id: "imageUpgraded",
+    name: "Image Generation Pro"
+  },
+  {
+    id: "textGeneration",
+    name: "Text Generation"
+  },
+  {
+    id: "textGenerationUpgraded",
+    name: "Text Generation Pro"
+  },
+  {
+    id: "imageClassification",
+    name: "Image Classification"
+  }
+];
