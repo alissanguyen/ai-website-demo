4cc45811e2ca68940b17c487fef86877913ed889
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-03 00:23:25 -0700

    Add homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 "use client"
 
+import HomePageAnimation from "@/components/HomePageAnimation/HomePageAnimation";
+
 export default function Home() {
 
 
   return (
-    <div className="w-full text-white p-10">
+    <div className="w-full text-white p-10 h-[inherit]">
+      <p className="Welcome__Text text-xl font-medium">Welcome.</p>
+      <HomePageAnimation />
+      <a href="/login">
+        <button className="HomePage__LoginButton rounded-full text-lg bg-slate-800 px-14 py-4 hover:text-cyan-300 hover:bg-slate-900/60 border-2 border-yellow-400/[0] hover:border-2 hover:border-cyan-400/[1] ease-in-out duration-200">Login</button>
+      </a>
+
     </div>
   );
 }
