975e40740052ebdcaf6e216eb356cb8458f17081
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-03 00:23:53 -0700

    Add avatar to user profile

--- src/app/account/account-form.tsx
+++ src/app/account/account-form.tsx
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useState } from 'react'
 import { createClient } from '@/utils/supabase/client'
 import { type User } from '@supabase/supabase-js'
 import "./AccountForm.css"
+import Avatar from './avatar'
 
 export default function AccountForm({ user }: { user: User | null }) {
   const supabase = createClient()
@@ -106,6 +107,15 @@ export default function AccountForm({ user }: { user: User | null }) {
               />
             </div>
           </div>
+          <Avatar
+            uid={user?.id ?? null}
+            url={avatar_url}
+            size={150}
+            onUpload={(url) => {
+              setAvatarUrl(url)
+              updateProfile({ fullname, username, website, avatar_url: url })
+            }}
+          />
           <div className='flex flex-row items-center justify-between'>
             <div className='p-3'>
               <button
