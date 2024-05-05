a12dedff8b60080c4d6825578e7b662fff25b3a0
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-05 16:18:49 -0700

    Add interface for userProfile returned from supabase query

--- src/types/types.ts
+++ src/types/types.ts
@@ -22,3 +22,12 @@ export interface Model {
     name: string
 }
 
+export interface UserProfile {
+    avatar_url: string | null;
+    conversations: CompleteConverse[] | null;
+    full_name: string | null;
+    id: string;
+    updated_at: string;
+    username: string | null;
+    website: string | null;
+}
