cc6ce6f8cc4a619cc0598417dafc45eb18ad10f2
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-05 21:12:47 -0700

    Add basic stylings to error page

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,3 +1,3 @@
 export default function ErrorPage() {
-    return <p>Sorry, something went wrong</p>
+    return <div className="mt-14 p-10"><p className="text-white">Sorry, something went wrong. Click <a href="/" className="text-cyan-300 underline underline-offset-2">here</a> to go back to home page.</p></div>
 }
